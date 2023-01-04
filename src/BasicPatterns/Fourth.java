4283bdd6344f2947219f995829f1d0fec2db649f
Siddhant Sharma <siddhantsharma909@gmail.com>
2023-01-04 11:56:51 +0530

    Fourth Pattern Added

---
+++ src/BasicPatterns/Fourth.java
@@ -0,0 +1,15 @@
+package BasicPatterns;
+
+public class Fourth {
+    public static void main(String[] args) {
+        int n = 5;
+        int st = n;
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=st; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            st--;
+        }
+    }
+}
