acb7a6a381e0c32762f349c63f74237861d567b0
Siddhant Sharma <siddhantsharma909@gmail.com>
2023-01-09 11:03:06 +0530

    Collection Started

---
+++ src/collections/NeedOfCollection.java
@@ -0,0 +1,16 @@
+package collections;
+
+public class NeedOfCollection {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+
+        int[] arr = new int[1000];
+        // limitation of array
+        // 1.  Fixed Size
+        // 2. Can Hold only homogeneous
+
+    }
+}
