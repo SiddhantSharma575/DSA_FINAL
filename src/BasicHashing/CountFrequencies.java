9766588f7964c0da81c5cefadc44c7d68ff34d05
Siddhant Sharma <siddhantsharma909@gmail.com>
2023-01-14 09:28:04 +0530

    Basic Hashing Problem Added

---
+++ src/BasicHashing/CountFrequencies.java
@@ -0,0 +1,23 @@
+package BasicHashing;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountFrequencies {
+    public static void main(String[] args) {
+        int[] arr  = {10,5,10,15,10,5};
+        Map<Integer,Integer> freqMap = new HashMap<>();
+        for (int ele : arr) {
+            if (!freqMap.containsKey(ele)){
+                freqMap.put(ele,1);
+            }else {
+                freqMap.put(ele, freqMap.get(ele) + 1);
+            }
+        }
+
+        for (Map.Entry<Integer,Integer> entry : freqMap.entrySet()){
+            System.out.println(entry.getKey() + " Occurs " + entry.getValue() + " times ");
+        }
+    }
+}
