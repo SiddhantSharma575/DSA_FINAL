8a1a2702c6920998c2779300babe6392248958ce
Siddhant Sharma <siddhantsharma909@gmail.com>
2023-02-20 20:31:29 +0530

    New Problems Added

---
+++ src/arrayproblems/medium/ReArrangeArray.java
@@ -0,0 +1,53 @@
+package arrayproblems.medium;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReArrangeArray {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n =  scn.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+
+        int[] ans = reArrange(arr);
+        for (int ele : ans){
+            System.out.print(ele + " ");
+        }
+    }
+
+    private static int[] reArrange(int[] arr) {
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+        for (int ele : arr) {
+            if (ele < 0) {
+                neg.add(ele);
+            }else {
+                pos.add(ele);
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean posS = true;
+        int i=0;
+        int j=0;
+        while (i<pos.size() || j < neg.size()){
+           if (posS){
+               ans.add(pos.get(i));
+               i++;
+               posS = false;
+           }else {
+               ans.add(neg.get(j));
+               j++;
+               posS = true;
+           }
+        }
+
+        int[] res = new int[arr.length];
+        for (int k=0; k<ans.size(); k++){
+            res[k] = ans.get(k);
+        }
+        return res;
+    }
+}
---
+++ src/arrayproblems/medium/StockBuyAndSell.java
@@ -0,0 +1,32 @@
+package arrayproblems.medium;
+
+import java.util.Scanner;
+
+public class StockBuyAndSell {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        int max =  Integer.MIN_VALUE;
+        for (int i=0; i<arr.length-1; i++){
+            for (int j=i+1; j<arr.length; j++) {
+                if (arr[i] < arr[j] && arr[j] - arr[i] > max) {
+                    max = arr[j] - arr[i];
+                }
+            }
+        }
+        System.out.println(max);
+    }
+    static int maxProfit(int[] arr) {
+        int maxPro = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            minPrice = Math.min(minPrice, arr[i]);
+            maxPro = Math.max(maxPro, arr[i] - minPrice);
+        }
+        return maxPro;
+    }
+}
