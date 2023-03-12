8da1ae1d113bd44c6d0f5f3e1f5cc1b6439802f5
Siddhant Sharma <siddhantsharma909@gmail.com>
2023-03-12 20:13:38 +0530

    Hard Problems Added

---
+++ src/arrayproblems/hard/LongSubZero.java
@@ -0,0 +1,15 @@
+package arrayproblems.hard;
+
+import java.util.Scanner;
+
+public class LongSubZero {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+    }
+}
