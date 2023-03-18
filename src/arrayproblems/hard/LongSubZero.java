89e1fb46bcb3b156ec676b31fc392bdfd87edbba
Siddhant Sharma <siddhantsharma909@gmail.com>
2023-03-18 18:17:28 +0530

    2 more hard problems added

--- src/arrayproblems/hard/LongSubZero.java
+++ src/arrayproblems/hard/LongSubZero.java
@@ -1,5 +1,6 @@
 package arrayproblems.hard;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class LongSubZero {
@@ -10,6 +11,46 @@ public class LongSubZero {
         for (int i=0; i<arr.length; i++) {
             arr[i] = scn.nextInt();
         }
+        // naive approach
+        sovle1(arr,n);
 
+        // Optimized Approach
+        solve2(arr,n);
+
+    }
+
+    private static void solve2(int[] arr, int n) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int i  = -1;
+        int sum = 0;
+        int maxlen = 0;
+        map.put(sum,i);
+        while (i < arr.length-1) {
+            i++;
+            sum += arr[i];
+            if (!map.containsKey(sum)) {
+                map.put(sum,i);
+            }else {
+                int len = i - map.get(sum);
+                if (len > maxlen) {
+                    maxlen = len;
+                }
+            }
+        }
+        System.out.println(maxlen);
+    }
+
+    private static void sovle1(int[] arr, int n) {
+        int max = 0;
+        for (int i=0; i<arr.length; i++) {
+            int sum = 0;
+            for (int j=i; j<arr.length; j++){
+                sum += arr[j];
+                if (sum == 0) {
+                    max = Math.max(max,j-i+1);
+                }
+            }
+        }
+        System.out.println(max);
     }
 }
